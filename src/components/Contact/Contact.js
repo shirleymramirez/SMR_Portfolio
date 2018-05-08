9e01a9bd1b9aeb75c37220b0e23c34ccf60c2db8
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-05-07 23:07:03 -0700

    added my email add at contactme page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./Contact.css";
 
 const Contact = () =>
-  <div className="container contact-me">
+  <div className="container">
     <div className="row">
-      <h1>
-        <u>Contact Info</u>
-      </h1>
+      <div className="connect-me">
+        <h4 className="connect-me-at">Connect with me at: </h4>
+        <a
+          href="mailto:shirley.mramirez@yahoo.com?Subject=Hello%20Shirley"
+          target="_top"
+        >
+          shirley.mramirez@yahoo.com
+        </a>
+      </div>
     </div>
   </div>;
 
