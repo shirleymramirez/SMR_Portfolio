1d77fa7579a3070abda18116220557aaa4419ad7
Shirley Ramirez <shirley.mramirez@yahoo.com>
2018-06-14 13:51:04 -0700

    deploying to gh pages

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -2,7 +2,6 @@ import React from "react";
 import Nav from "../../components/Nav/Nav";
 import PortfolioComponent from "../../components/Portfolio/Portfolio";
 import Footer from "../../components/Footer/Footer";
-import "./Portfolio.css";
 
 const Portfolio = () =>
   <div>
